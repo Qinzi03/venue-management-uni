97678be251d4f052e5afed930c91634e59eefefc
Qinzi03 <1033269929@qq.com>
2025-03-15 21:16:09 +0800

    feat: 去掉加入场馆的功能

--- src/pages/venue/index.vue
+++ src/pages/venue/index.vue
@@ -1,15 +1,10 @@
 <template>
   <view>
-    <uv-sticky bgColor="#fff">
-      <uv-tabs
-        :list="tabList"
-        :current="current"
-        @click="onClick"
-        :scrollable="false"
-      ></uv-tabs>
-    </uv-sticky>
-
-    <uv-list v-if="!current" class="marginT20" :itemStyle="{ height: '50px' }">
+    <uv-list
+      v-if="createList.length"
+      class="marginT20"
+      :itemStyle="{ height: '50px' }"
+    >
       <uv-list-item v-for="item in createList" :key="item.id">
         <uv-cell
           :title="item.name"
@@ -24,67 +19,14 @@
         </uv-cell>
       </uv-list-item>
     </uv-list>
-    <uv-list v-if="current === 1" class="marginT20">
-      <uv-list-item v-for="item in joinedList" :key="item.id">
-        <uv-cell
-          :title="item.name"
-          :border="false"
-          :customStyle="{
-            padding: '6px 6px',
-            'border-bottom': '1px solid #d6d7d9',
-          }"
-        >
-        </uv-cell>
-      </uv-list-item>
-    </uv-list>
-
-    <view class="add" @click="onAdd">
-      <uv-icon name="plus" size="30" color="#ffffff"></uv-icon>
+    <view class="empty" v-else>
+      <uv-empty
+        mode="list"
+        text="您还没有创建的场馆，请点击右下角进行创建"
+      ></uv-empty>
     </view>
+    <view class="add" @click="onAdd"> 创建场馆 </view>
     <ve-footer tabName="venue"> </ve-footer>
-
-    <!-- 选择新增类型 -->
-    <uv-action-sheet
-      ref="actionSheet"
-      :actions="actionList"
-      title="选择类型"
-      @select="onSelect"
-    >
-    </uv-action-sheet>
-
-    <!-- 加入场馆 -->
-    <uv-modal ref="modal" title="加入场馆">
-      <uv-form
-        labelPosition="left"
-        :model="joinedForm"
-        :rules="rules"
-        ref="joinFormEl"
-        labelWidth="100"
-      >
-        <uv-form-item label="场馆名称" prop="name" borderBottom>
-          <uv-input v-model="joinedForm.name" border="none"> </uv-input>
-        </uv-form-item>
-      </uv-form>
-      <template #confirmButton>
-        <view class="flex marginB20">
-          <view class="marginR20">
-            <uv-button
-              text="取消"
-              size="small"
-              @click="onCancelJoin"
-            ></uv-button
-          ></view>
-          <view class="marginR20">
-            <uv-button
-              text="确定"
-              type="primary"
-              size="small"
-              @click="onSubmit"
-            ></uv-button>
-          </view>
-        </view>
-      </template>
-    </uv-modal>
 
     <!-- 合伙人管理 -->
     <uv-modal ref="partnerModal" title="合伙人管理">
@@ -129,76 +71,28 @@
   </view>
 </template>
 <script setup>
-import { reactive, ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
+import { myVenue } from "@/config/api.js";
+onMounted(() => {
+  getListData();
+});
 
-const tabList = ref([
-  {
-    name: "我创建的",
-  },
-  {
-    name: "我加入的",
-  },
-]);
-
-const current = ref(0);
-const onClick = ({ index }) => {
-  console.log("----onClick", index);
-  current.value = index;
+const getListData = async () => {
+  const res = await myVenue();
+  createList.value = res || [];
 };
 
-const createList = ref([
-  {
-    id: 1,
-    onLineNum: 1,
-    name: "春来花几枝",
-  },
-  {
-    id: 2,
-    onLineNum: 12,
-    name: "天下谁人不识君",
-  },
-]);
-const joinedList = ref([
-  {
-    id: 1,
-    name: "上岸",
-  },
-  {
-    id: 2,
-    name: "nice",
-  },
-]);
+const createList = ref([]);
 
-const actionList = ref([
-  {
-    name: "创建场馆",
-    id: 1,
-  },
-  {
-    name: "加入场馆",
-    id: 2,
-  },
-]);
 // 选择新增类型
 const actionSheet = ref();
 const modal = ref();
 const onAdd = () => {
-  actionSheet.value.open();
-};
-const onSelect = ({ id }) => {
-  if (id === 2) {
-    modal.value.open();
-    return;
-  }
   uni.navigateTo({
     url: `/pages/createVenue/index`,
   });
 };
 
-// 加入场馆
-const joinedForm = reactive({
-  name: "",
-});
 const rules = reactive({
   name: {
     type: "string",
@@ -207,22 +101,6 @@ const rules = reactive({
     trigger: ["blur", "change"],
   },
 });
-const onCancelJoin = () => {
-  modal.value.close();
-};
-const joinFormEl = ref();
-
-const onSubmit = async () => {
-  const res = await joinFormEl.value.validate();
-  console.log(res);
-  uni.showToast({
-    icon: "success",
-    title: "提交成功",
-  });
-  setTimeout(() => {
-    onCancelJoin();
-  }, 2000);
-};
 
 const partnerModal = ref();
 const partnerFormEl = ref();
@@ -262,8 +140,8 @@ const onDelPartner = async () => {
 <style lang="scss" scoped>
 .add {
   position: fixed;
-  width: 60px;
-  height: 60px;
+  width: 40px;
+  height: 40px;
   border-radius: 60px;
   background-color: #1989fa;
   bottom: 120px;
@@ -271,6 +149,9 @@ const onDelPartner = async () => {
   display: flex;
   align-items: center;
   justify-content: center;
+  color: white;
+  padding: 10px;
+  text-align: center;
 }
 .flex {
   display: flex;
@@ -279,5 +160,12 @@ const onDelPartner = async () => {
 }
 .marginT20 {
   margin-top: 20px;
+}
+.empty {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-left: -140px;
+  margin-top: -80px;
 }
 </style>
